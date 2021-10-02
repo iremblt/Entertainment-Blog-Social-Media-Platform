e723a0f90fd3ce1e64bb2bc77eb543ac1e530dbd
iremblt <85560757+iremblt@users.noreply.github.com>
2021-10-02 22:17:49 +0300

    Add project files.

---
+++ Entertainment Blog ~Social Media Platform.UI/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Entertainment_Blog__Social_Media_Platform.UI.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
